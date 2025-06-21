f4860a0fcc909422d29181165728b9ea253d7407
William Studer <bill.studer@sent.com>
2025-06-20 21:42:08 -0400

    ch3 simple bounded sorted list

--- src/main/java/com/studerw/datastructures/ch3/Scoreboard.java
+++ src/main/java/com/studerw/datastructures/ch3/Scoreboard.java
@@ -19,45 +19,54 @@ public class Scoreboard {
         this.board = new GameEntity[capacity];
     }
 
-    public void add(GameEntity entity) {
-        // empty
-        if (numEntities == 0) {
-            board[0] = entity;
-            ++numEntities;
-            return;
-        }
-
-        // full and current is not less than least high score
-        if (board.length == numEntities && board[numEntities - 1].getScore() < entity.getScore()) {
-            log.warn("Entity: {} is not less than low score: {}, not adding to list.", entity, board[numEntities - 1]);
-        }
-
-        // not empty but not full
-        // if (board.length > numEntities) {
-        int current = numEntities;
-        while (current > 0 && entity.getScore() < board[current - 1].getScore()) {
-            // we can't shove the last place entity outside the index of the array, so it just gets thrown away
-            if (current < board.length) {
-                board[current] = board[current - 1];
-            }
-
-            current--;
-        }
-        // if this one was inserted into the list
-        if (current != numEntities) {
-            board[current] = entity;
-        }
-        if (board.length > numEntities && current != numEntities) {
-            numEntities++;
-        }
-    }
-
     public int getNumEntities() {
         return numEntities;
     }
 
     public GameEntity[] getBoard() {
         return board;
+    }
+
+    /**
+     *
+     * @param entity add to the board if a slot exists or if the entity.score is greater than the current lowest score in the list. Otherwise, the entity is not added.
+     */
+    public void add(GameEntity entity) {
+        // if the board is full and the entity.score is less than or equal to the least high score, then just return
+        if (board.length == numEntities && entity.getScore() <= board[numEntities - 1].getScore()) {
+            log.info("Entity: {} is not greater than low score: {}, not adding to list.", entity, board[numEntities - 1]);
+            return;
+        }
+
+        // if there are still board slots open, then we know we're increasing the numEntities,
+        // and this also allows us to set the comparison pointer to the correct slot - either the first empty slot if slots still exist
+        // or the last slot (we know that we're higher than this slot, which we're bumping out of the list, due to the if check above).
+        if (numEntities < board.length) {
+            numEntities++;
+        }
+
+        // We set the current pointer to the "end" of the list based on the numEntities.
+        // Due to the IF comparison right above, we know we're comparing it to the last entity if there are still slots open (which we're pointing to now due to increasing numEntities),
+        // or if the board is full, then we know we're higher than the last one due to the first IF, and so we can set the pointer to that (due to numEntities not increasing) and begin to compare with
+        // the rest of the list's entities since we KNOW the current end entity is being bumped off.
+        int ptr = numEntities - 1;
+        while (ptr > 0 && entity.getScore() > board[ptr - 1].getScore()) {
+            board[ptr] = board[ptr - 1];
+            ptr--;
+        }
+        // if we got here, we know we're inserting the entity wherever the WHILE loop left off
+        board[ptr] = entity;
+    }
+
+    /**
+     *
+     * @param i index of board to remove
+     */
+    public void remove(int i) {
+        if (i < 0 || i >= (numEntities -1)) {
+            log.warn("Cannot remove index: {} from a board of {} entities.",  i, numEntities);
+            throw new IllegalArgumentException("Index out of range");
+        }
     }
 
     @Override
--- src/test/java/com/studerw/datastructures/ch3/ScoreboardTest.java
+++ src/test/java/com/studerw/datastructures/ch3/ScoreboardTest.java
@@ -21,7 +21,7 @@ class ScoreboardTest {
     }
 
     @Test
-    void testEmptyGameboard() {
+    void addToEmptyGameboard() {
        final Scoreboard scoreboard =  new Scoreboard(3);
        scoreboard.add(new GameEntity("bill", 1));
        assertThat(scoreboard.getNumEntities()).isEqualTo(1);
@@ -29,44 +29,141 @@ class ScoreboardTest {
     }
 
     @Test
-    void testPartiallyFullGameboard() {
+    void addToPartiallyFullGameboard() {
         final Scoreboard scoreboard =  new Scoreboard(4);
         scoreboard.add(new GameEntity("bill", 5));
         scoreboard.add(new GameEntity("bob", 1));
 
         log.debug("{}", scoreboard);
         assertThat(scoreboard.getNumEntities()).isEqualTo(2);
-        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bob");
-        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("bob");
 
         log.debug("{}", scoreboard);
         scoreboard.add(new GameEntity("james", 3));
         assertThat(scoreboard.getNumEntities()).isEqualTo(3);
-        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bob");
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
         assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
-        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
     }
 
     @Test
-    void testFullGameboard() {
+    void addToFullGameboard() {
         final Scoreboard scoreboard =  new Scoreboard(3);
         scoreboard.add(new GameEntity("bill", 5));
-        scoreboard.add(new GameEntity("james", 3));
         scoreboard.add(new GameEntity("bob", 1));
+        scoreboard.add(new GameEntity("james", 3));
 
         log.debug("{}", scoreboard);
         assertThat(scoreboard.getNumEntities()).isEqualTo(3);
-        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bob");
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
         assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
-        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
 
         scoreboard.add(new GameEntity("mary", 4));
         log.debug("{}", scoreboard);
         assertThat(scoreboard.getNumEntities()).isEqualTo(3);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("mary");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("james");
+
+        scoreboard.add(new GameEntity("larry", 3));
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("mary");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("james");
+    }
+
+    @Test
+    void addToFullGameboardThatIsLessThanLowScore() {
+        final Scoreboard scoreboard =  new Scoreboard(3);
+        scoreboard.add(new GameEntity("bill", 5));
+        scoreboard.add(new GameEntity("bob", 2));
+        scoreboard.add(new GameEntity("james", 3));
+
+        log.debug("{}", scoreboard);
         assertThat(scoreboard.getNumEntities()).isEqualTo(3);
-        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bob");
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
         assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
-        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("mary");
-        assertThat(scoreboard.getBoard()[2].getScore()).isEqualTo(4);
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
+
+        scoreboard.add(new GameEntity("mary", 2));
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(3);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
+
+        scoreboard.add(new GameEntity("larry", 1));
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
+    }
+
+    @Test
+    public void removeInvalidIndex(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            final Scoreboard scoreboard =  new Scoreboard(3);
+            scoreboard.remove(0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            final Scoreboard scoreboard =  new Scoreboard(3);
+            scoreboard.remove(3);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            final Scoreboard scoreboard =  new Scoreboard(1);
+            scoreboard.add(new GameEntity("larry", 1));
+            scoreboard.remove(3);
+        });
+    }
+
+    @Test
+    public void removeFromPartiallyFull() {
+        final Scoreboard scoreboard =  new Scoreboard(4);
+        scoreboard.add(new GameEntity("bill", 5));
+        scoreboard.add(new GameEntity("bob", 2));
+        scoreboard.add(new GameEntity("james", 3));
+
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(3);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
+
+        scoreboard.remove(0);
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(2);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("james");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("bob");
+    }
+
+    @Test
+    public void removeFromFullGameboard() {
+        final Scoreboard scoreboard =  new Scoreboard(4);
+        scoreboard.add(new GameEntity("bill", 5));
+        scoreboard.add(new GameEntity("bob", 2));
+        scoreboard.add(new GameEntity("mary", 4));
+        scoreboard.add(new GameEntity("james", 3));
+
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(4);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("mary");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("james");
+        assertThat(scoreboard.getBoard()[2].getName()).isEqualTo("bob");
+
+        scoreboard.remove(2);
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(3);
+        assertThat(scoreboard.getBoard()[0].getName()).isEqualTo("bill");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("mary");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("bob");
+
+        scoreboard.remove(0);
+        log.debug("{}", scoreboard);
+        assertThat(scoreboard.getNumEntities()).isEqualTo(2);
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("mary");
+        assertThat(scoreboard.getBoard()[1].getName()).isEqualTo("bob");
     }
 }
